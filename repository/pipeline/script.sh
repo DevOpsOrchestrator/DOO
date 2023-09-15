84d97eeb620de72be213ccaa03e891ab1b5e0c7b
Thiago de Abreu Lima <thiagoabreulima@gmail.com>
2023-09-15 16:36:59 -0300

    update pipeline

--- repository/pipeline/script.sh
+++ repository/pipeline/script.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-IFS=$(echo -en "\n")
-for file in *.yaml; 
-do 
-    ansible-playbook -i hosts $file --vault-id @vault.py
+
+# Loop através de todos os arquivos .yml e .yaml no diretório
+for arquivo in *.yaml *.yml; do
+    # Verifica se o arquivo não é oculto
+    if [[ ! $arquivo == *"/."* ]]; then
+        # Verifica se o arquivo é um arquivo regular (não um diretório)
+        if [[ -f $arquivo ]]; then
+            # Executa o arquivo
+            echo executing $arquivo
+            ansible-playbook -i hosts $arquivo --vault-id @vault.py
+        fi
+    fi
 done
