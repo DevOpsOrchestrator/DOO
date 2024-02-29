fffd887110af30fb18dd58a03c98570559d39ed8
Thiago de Abreu Lima <thiagoabreulima@gmail.com>
2024-02-29 11:56:59 -0300

    começando docs DNS

--- docs/containernet/DNS/dns_containernet.py
+++ docs/containernet/DNS/dns_containernet.py
@@ -20,30 +20,30 @@ def topology():
 
     info('*** Adding docker containers\n')
 
-    d1 = net.addDocker('postgres', ip='10.0.0.250', dimage="thiagoabreulima/postgres_validacao:latest",
+    d1 = net.addDocker('postgres', ip='10.0.0.240', dimage="thiagoabreulima/postgres_validacao:latest",
                        port_bindings={5432: 5432}, volumes=["doo-postgres-data:/var/lib/postgresql/data"],
                        environment={"POSTGRES_DB": "doo",
                                     "POSTGRES_PASSWORD": "postgres",
                                     "POSTGRES_USER": "postgres"})
-    
-    d2 = net.addDocker('gitlab', ip='10.0.0.251', dimage="gitlab/gitlab-ce:latest",
-                       port_bindings={80: 80, 443: 443},
-                       environment={'GITLAB_OMNIBUS_CONFIG':"external_url 'http://10.0.0.251'",
-                                    'GITLAB_ROOT_PASSWORD':'doo$123456',
-                                    'GITLAB_SHARED_RUNNERS_REGISTRATION_TOKEN':'j5ZKrgztV5Qm9Cyg5ZFz'},
-                       volumes=["config:/etc/gitlab", "logs:/var/log/gitlab", "data:/var/opt/gitlab"])
 
-    d3 = net.addDocker('runner', ip='10.0.0.252', dimage="thiagoabreulima/gitlab_runner:latest",
-                       environment={"CI_SERVER_URL": "10.0.0.251",
+    d2 = net.addDocker('gitlab', ip='10.0.0.241', dimage="gitlab/gitlab-ce:latest",
+                       port_bindings={80: 80, 443: 443},
+                       environment={'GITLAB_OMNIBUS_CONFIG':"external_url 'http://10.0.0.241'",
+                                    'GITLAB_ROOT_PASSWORD':'doo$654321',
+                                    'GITLAB_SHARED_RUNNERS_REGISTRATION_TOKEN':'j5ZKrgztV5Qm9Cyg5ZFz'},
+                       volumes=["/etc/gitlab", "/var/log/gitlab", "/var/opt/gitlab"])
+
+    d3 = net.addDocker('runner', ip='10.0.0.242', dimage="thiagoabreulima/gitlab_runner:latest",
+                       environment={"CI_SERVER_URL": "10.0.0.241",
                                     "REGISTRATION_TOKEN": "j5ZKrgztV5Qm9Cyg5ZFz"},
-                       volumes=["runner:/etc/gitlab-runner", "sock:/var/run/docker.sock"])
-    
-    d4 = net.addDocker('doo', ip='10.0.0.253', dimage="devopsorchestrator/doo:latest",
+                       volumes=["/etc/gitlab-runner", "/var/run/docker.sock"])
+
+    d4 = net.addDocker('doo', ip='10.0.0.243', dimage="devopsorchestrator/doo:latest",
                        port_bindings={8000: 8000},
                        environment={"DB_NAME": "doo",
                                     "DB_PASSWORD": "postgres",
                                     "DB_USER": "postgres",
-                                    "DB_HOST": "10.0.0.250",
+                                    "DB_HOST": "10.0.0.240",
                                     "DEBUG": True,
                                     "DB_WAIT_DEBUG": 1,
                                     "SKIP_SUPERUSER": "false",
@@ -53,10 +53,20 @@ def topology():
                                     "SUPERUSER_PASSWORD": "admin"},
                         volumes=["repositorio:/home/app/repository",])
 
-    d5 = net.addDocker('apache1', ip='10.0.0.254', volumes=['/tmp/.X11-unix:/tmp/.X11-unix:rw'],
+    d5 = net.addDocker('apache1', ip='10.0.0.244', volumes=['/tmp/.X11-unix:/tmp/.X11-unix:rw'],
                        privileged=True, environment={'DISPLAY': ":0"},
                        dimage="thiagoabreulima/validacao:latest",
-                       publish_all_ports=True)
+                       port_bindings={8080: 80, 9922: 22})
+    
+    d6 = net.addDocker('bind9', ip='10.0.0.245', volumes=['/tmp/.X11-unix:/tmp/.X11-unix:rw'],
+                       privileged=True, environment={'DISPLAY': ":0"},
+                       dimage="thiagoabreulima/validacao:latest",
+                       port_bindings={9953: 53, 2222: 22})
+    
+    d7 = net.addDocker('client', ip='10.0.0.246', volumes=['/tmp/.X11-unix:/tmp/.X11-unix:rw'],
+                       privileged=True, environment={'DISPLAY': ":0"},
+                       dimage="thiagoabreulima/validacao:latest",
+                       port_bindings={4622: 22})
 
     info('*** Adding switch\n')
     s1 = net.addSwitch('s1', failMode='standalone')
@@ -67,6 +77,8 @@ def topology():
     net.addLink(d3, s1)
     net.addLink(d4, s1)
     net.addLink(d5, s1)
+    net.addLink(d6, s1)
+    net.addLink(d7, s1)
 
     info('*** Starting network\n')
     net.start()
@@ -76,6 +88,8 @@ def topology():
     d3.cmd('gitlab-runner start &')
     d4.cmd('/home/app/doo/docker-entrypoint.sh &')
     d5.cmd('service ssh start')
+    d6.cmd('service ssh start')
+    d7.cmd('service ssh start')
 
     info('*** Running CLI\n')
     CLI(net)
